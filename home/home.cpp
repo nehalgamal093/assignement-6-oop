168e4298415db17c5b28a43d182f42c038fbc067
Nehal Gamal <nehalgamal093@gmail.com>
2024-08-24 20:36:11 +0300

    Circle class "Encapsulation"

--- home/home.cpp
+++ home/home.cpp
@@ -1,7 +1,61 @@
+//Task 1
 #include <iostream>
 using namespace std;
 
+class Circle {
+	double radius;
+	string color;
+public:
+	Circle() {
+		radius = 1.0;
+		color = "red";
+	}
+	Circle(double radius) {
+		this->radius = radius;
+	}
+	Circle(double radius, string color) {
+		this->radius = radius;
+		this->color = color;
+	}
+	double getRadius() {
+		return radius;
+	}
+	string getColor() {
+		return color;
+	}
+	void setRadius(double radius) {
+		this->radius = radius;
+	}
+	void setColor(string color) {
+		if (radius > 0) {
+			this->color = color;
+		}
+	}
+	double getArea() {
+		return 3.14 * radius * radius;
+	}
+};
 int main() {
-	
+	//Default Constructor
+	Circle circle;
+	cout <<	"Circle " << circle.getColor() << endl;
+	cout << "Circle " << circle.getRadius() << endl;
+	cout << "Circle " << circle.getArea() << endl;
+	cout << "======" << endl;
+
+	//Parameterized Constructor with two parameters radius and color
+	Circle circle1(2, "blue");
+	cout << "Circle 1 " << circle1.getColor() << endl;
+	cout << "Circle 1 " << circle1.getRadius() << endl;
+	cout << "Circle 1 " << circle1.getArea() << endl;
+	cout << "======" << endl;
+
+	//Parameterized Constructor with one parameter radius 
+	Circle circle2(3);
+	circle2.setColor("yellow");
+	cout << "Circle 2 " << circle2.getColor() << endl;
+	cout << "Circle 2 " << circle2.getRadius() << endl;
+	cout << "Circle 2 " << circle2.getArea() << endl;
+	cout << "======" << endl;
 	return 0;
 }
